2fcf2a1e7af00948aa78e751335bca93c490b7b7
nicky-eng <nicolasoyarzabal@gmail.com>
2021-08-30 19:13:00 -0300

    Refactored pagination logic to avoid code repetition

--- src/app/store-list/store-list.component.ts
+++ src/app/store-list/store-list.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { delay } from 'rxjs/operators';
 
-import { Store } from '../store';
+import { Store, Pagination } from '../store';
 import { StoresService } from '../stores.service';
 import { LoadingService } from '../loading.service';
 
@@ -19,7 +19,10 @@ export class StoreListComponent implements OnInit {
   paginationPrevious: string = '';
   numberOfPages: number;
   currentPage: number;
-  pagesShown: any[] = []; //TODO: Replace this with a class
+  pagesShown: Pagination[] = [];
+  initializer: number;
+  endCondition: number; // Sets the final number of pages to paginate.
+  paginationAddress: string;
 
 
   getStores(url?: string) {
@@ -27,62 +30,80 @@ export class StoreListComponent implements OnInit {
       this.storeList = data['results'];
       this.paginationNext = data['next'];
       this.paginationPrevious = data['previous'];
-      this.numberOfPages = Math.ceil(data['count'] / 1);
+      this.numberOfPages = Math.ceil(data['count'] / 5);
 
       console.log(this.paginationNext)
 
       // Extraction ofthe current page number from "next page" link
       if (this.paginationNext !== null) {
         this.currentPage = Number(this.paginationNext.split('page=')[1]) - 1;
+        this.paginationAddress = this.paginationNext.split('page=')[0];
       } else {
         this.currentPage = this.numberOfPages;
       }
+
+
+
+      this.pagesShown = []; // We reset the list on every API call
+      // this.initializer = Math.max(1, this.currentPage - 2) <= 0 ? 1 :
+      //   this.currentPage - 2;
+      // this.endCondition = Math.min(this.currentPage + 2, this.numberOfPages)
+
 
       // Selection of the page numbers to be displayed on the pagination nav
 
       this.pagesShown = []; // We reset the list on every API call
 
-      if (this.numberOfPages <= 5 || [1, 2].includes(this.currentPage)) {
-        for (let i = 1; i < (this.numberOfPages + 1); i++) {
-          this.pagesShown.push({
-            "number": i,
-            "address": `http://localhost:8000/stores/?format=json&page=${i}`
-          })
+      // Set initializer and end condition for pages loop
 
-          if (this.pagesShown.length == 5) {
-            break
-          }
-          console.log("current Page", this.currentPage)
-          console.log(i);
-          console.log(this.pagesShown);
-        }
+      if (this.numberOfPages <= 5 || [1, 2].includes(this.currentPage)) {
+        this.initializer = 1;
+        this.endCondition = Math.min(this.numberOfPages, 5);
       } else if ([this.numberOfPages, this.numberOfPages - 1].includes(
         this.currentPage)) {
-        for (let i = (this.currentPage - 4);
-          i < this.numberOfPages + 1; i++) {
-          this.pagesShown.push({
-            "number": i,
-            "address": `http://localhost:8000/stores/?format=json&page=${i}`
-          })
-
-          console.log("current Page", this.currentPage)
-          console.log(i);
-          console.log(this.pagesShown);
-        }
+        this.initializer = this.currentPage - 4;
+        this.endCondition = this.numberOfPages;
       } else {
-        for (let i = this.currentPage - 2; i < this.currentPage + 3; i++) {
-          this.pagesShown.push({
-            "number": i,
-            "address": `http://localhost:8000/stores/?format=json&page=${i}`
-          })
-
-          console.log("current Page", this.currentPage)
-          console.log(i);
-          console.log(this.pagesShown);
-
-
-        }
+        this.initializer = this.currentPage - 2;
+        this.endCondition = this.currentPage + 2;
       }
+      for (let i = this.initializer; i <= this.endCondition; i++) {
+        this.pagesShown.push({
+          "number": i,
+          "address": `${this.paginationAddress}page=${i}`
+        })
+      }
+
+
+
+      // if (this.numberOfPages <= 5 || [1, 2].includes(this.currentPage)) {
+      //   for (let i = 1; i < (this.numberOfPages + 1); i++) {
+      //     this.pagesShown.push({
+      //       "number": i,
+      //       "address": `http://localhost:8000/stores/?format=json&page=${i}`
+      //     })
+
+      //     if (this.pagesShown.length == 5) {
+      //       break
+      //     }
+      //   }
+      // } else if ([this.numberOfPages, this.numberOfPages - 1].includes(
+      //   this.currentPage)) {
+      //   for (let i = (this.currentPage - 4);
+      //     i < this.numberOfPages + 1; i++) {
+      //     this.pagesShown.push({
+      //       "number": i,
+      //       "address": `http://localhost:8000/stores/?format=json&page=${i}`
+      //     })
+      //   }
+      // } else {
+      //   for (let i = this.currentPage - 2; i < this.currentPage + 3; i++) {
+      //     this.pagesShown.push({
+      //       "number": i,
+      //       "address": `http://localhost:8000/stores/?format=json&page=${i}`
+      //     })
+      //   }
+      // }
     });
   }
 
