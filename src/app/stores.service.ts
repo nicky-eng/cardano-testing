1d523d6664d361634c3f3b99a528b1565f6dda0d
nicky-eng <nicolasoyarzabal@gmail.com>
2021-09-01 19:42:40 -0300

    Trying to catch a timeout error

--- src/app/stores.service.ts
+++ src/app/stores.service.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
-import { Observable } from 'rxjs';
+import { Observable, throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
 
 
 @Injectable({
@@ -11,14 +12,26 @@ export class StoresService {
 
   constructor(private http: HttpClient) { }
 
-  //private storesUrl = 'http://localhost:8000/stores/?format=json';
+  private storesUrl = 'http://example.com:81/';
   // private storesUrl = 'https://testing-cardano-back.herokuapp.com/stores/?format=json';
-  private storesUrl = 'https://cardano-directory-back.herokuapp.com/stores/?format=json';
+  // private storesUrl = 'https://cardano-directory-back.herokuapp.com/stores/?format=json';
 
   getStores(url: string): Observable<any> {
     const currentUrl = url ?? this.storesUrl
 
-    return this.http.get(currentUrl)
+    return this.http.get(currentUrl).pipe(
+      catchError((err) => {
+        debugger;
+        if (err.status === 0) {
+          debugger;
+          return this.getStores(url)
+        } else {
+          console.log('error caught in service')
+          console.error(err);
+          return throwError(err);
+        }
+      })
+    )
   }
 
 
